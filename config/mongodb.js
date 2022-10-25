16b9e60b1fda3784315a50ee2fef7559d2260de7
TheTitan-X <104201860+TheTitan-X@users.noreply.github.com>
2022-10-25 19:40:16 -0400

    Addes engines

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,3 +1,3 @@
 module.exports = {
-  URI: "mongodb+srv://admin:5XfYrvPKBroUmvF9@project.nihhh7u.mongodb.net/?retryWrites=true&w=majority",
+  URI: "mongodb+srv://aditya:eWj2oHAfVoBg94Xa@mongodb.qmszfox.mongodb.net/?retryWrites=true&w=majority",
 };
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./config/server");
 
 //app.setMaxListeners;
 
-app.listen(process.env.PORT || 3001, () => console.log('App started...'));
+app.listen(process.env.PORT || 3000, () => console.log('App started...'));
 
